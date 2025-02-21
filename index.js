feeb1faaf3c16ee1ef12864d9cbd7113ac2a8e3b
AbuSufiyan19 <abusufiyan8800@gmail.com>
2025-02-21 09:29:05 +0530

    IP changes

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.use((req, res, next) => {
     console.log(`Client IP: ${clientIp}`);
 
     if (clientIp !== ALLOWED_PUBLIC_IP) {
-        return res.status(403).json({ error: "Access Denied. Connect to the allowed WiFi." });
+        return res.status(403).json({ error: "Access Denied. Connect to the college WiFi." });
     }
 
     next();
