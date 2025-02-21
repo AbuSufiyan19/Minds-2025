f31a4106eb2b3791ac0c61cd824c8ebaebb5dd1a
AbuSufiyan19 <abusufiyan8800@gmail.com>
2025-02-21 14:52:29 +0530

    final changes

--- index.js
+++ index.js
@@ -48,19 +48,19 @@ app.use((req, res, next) => {
     next();
 });
 
-// const ALLOWED_PUBLIC_IPS = ["14.139.180.67", "103.224.33.35"]; 
-// // Middleware to restrict access based on Public IP
-// app.use((req, res, next) => {
-//     // Extract the first IP from x-forwarded-for (public IP)
-//     const clientIp = req.headers["x-forwarded-for"]?.split(",")[0].trim() || req.connection.remoteAddress;
+const ALLOWED_PUBLIC_IPS = ["14.139.180.67", "103.224.33.35"]; 
+// Middleware to restrict access based on Public IP
+app.use((req, res, next) => {
+    // Extract the first IP from x-forwarded-for (public IP)
+    const clientIp = req.headers["x-forwarded-for"]?.split(",")[0].trim() || req.connection.remoteAddress;
 
-//     console.log(`Client IP: ${clientIp}`);
+    console.log(`Client IP: ${clientIp}`);
 
-//     if (!ALLOWED_PUBLIC_IPS.includes(clientIp)) {
-//         return res.status(403).json({ error: "Access Denied. Connect to the college WiFi." });
-//     }
-//     next();
-// });
+    if (!ALLOWED_PUBLIC_IPS.includes(clientIp)) {
+        return res.status(403).json({ error: "Access Denied. Connect to the college WiFi." });
+    }
+    next();
+});
 
 
 const userRoutes = require('./Routes/userRoutes');
